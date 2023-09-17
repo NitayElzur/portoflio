fc9154ae716424add585b60fc6d01496f351048a
Nitay Elzur <elzur.nitay@gmail.com>
2023-09-17 13:32:27 +0300

    added nahal project

---
+++ src/Pages/Projects/Data/AmoutatHaNahal.js
@@ -0,0 +1,16 @@
+import video from '../../../assets/amoutat-hanahal.webm'
+export const data = {
+    title: 'The Nahal Brigade Association',
+    subtitle: 'A site showcasing all the work that is put to help Nahal\'s in and out of service soldiers',
+    role: 'Frontend Developer',
+    role_desc: 'Creating the layout of the webpage',
+    tech_stack: [
+        'React'
+    ],
+    links: {
+        GitHub: 'https://github.com/NitayElzur/Nahal',
+        Site: 'https://amoutat-hanahal.netlify.app/'
+    },
+    page_route: '',
+    video
+}
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -2,11 +2,13 @@ import './Projects.css'
 import SpecificProject from './SpecificProject/SpecificProject';
 import { data as app2u } from './Data/App2U';
 import {data as confrence} from './Data/Confrence';
+import {data as nahal} from './Data/AmoutatHaNahal';
 
 function Projects() {
     return (
         <div id="projects-main">
             <SpecificProject data={app2u}/>
+            <SpecificProject data={nahal}/>
             <SpecificProject data={confrence}/>
         </div>
     )
